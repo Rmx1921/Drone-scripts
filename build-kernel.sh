33db56ddb60c80e6d2309b8e44d14db84d0e44ae
viciouspup <yadhuuk@gmail.com>
2021-01-13 13:47:55 +0530

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -150,6 +150,7 @@ cd $(pwd)/${OUT_DIR}/arch/arm64/boot/
 else
 	exit 1;
 fi
+cd $(pwd)
 cd $(pwd)/libufdt-master-utils/src
 python mkdtboimg.py create /home/user/davinci-p-oss/out/arch/arm64/boot/dtbo.img
 cd $(pwd)/${OUT_DIR}/arch/arm64/boot/
