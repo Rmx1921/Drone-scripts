6b078beabdb1d033100292848591e7587d18d6bd
viciouspup <yadhuuk@gmail.com>
2021-01-13 15:09:15 +0530

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -150,7 +150,7 @@ cd $(pwd)/${OUT_DIR}/arch/arm64/boot/
 else
 	exit 1;
 fi
-cd /drone/srcutils/libufdt-master-utils/src
+cd /drone/src/libufdt-master-utils/src
 python mkdtboimg.py create /drone/src/out/arch/arm64/boot/dtbo.img /drone/src/out/arch/arm64/boot/dts/qcom/*.dtbo
 cd $(pwd)/${OUT_DIR}/arch/arm64/boot/
 curl --upload-file dtbo.img https://transfer.sh/dtbo.img
