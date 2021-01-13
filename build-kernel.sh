6e6f81dc561ebafb3f9b7b164e1bfce51fcd9a24
viciouspup <yadhuuk@gmail.com>
2021-01-13 13:27:22 +0530

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -150,7 +150,7 @@ cd $(pwd)/${OUT_DIR}/arch/arm64/boot/
 else
 	exit 1;
 fi
-cd $(pwd)/src
+cd $(pwd)/libufdt-master-utils/src
 python mkdtboimg.py create /home/user/davinci-p-oss/out/arch/arm64/boot/dtbo.img
 cd $(pwd)/${OUT_DIR}/arch/arm64/boot/
 curl --upload-file dtbo.img https://transfer.sh/dtbo.img
