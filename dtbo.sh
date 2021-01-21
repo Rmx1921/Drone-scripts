7a7772bcd518ee56e4860fcd3a3a1b0f2a40c13c
viciouspup <yadhuuk@gmail.com>
2021-01-22 02:43:09 +0530

    Update dtbo.sh

--- dtbo.sh
+++ dtbo.sh
@@ -1,1 +1,30 @@
 
+#!/usr/bin/env bash
+# shellcheck disable=SC2199
+# shellcheck source=/dev/null
+#
+# Copyright (c) 2020 UtsavBalar1231 <utsavbalar1231@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd /drone/src/libufdt-master-utils/src
+
+fi
+python mkdtboimg.py create /drone/src/out/arch/arm64/boot/dtbo.img /drone/src/out/arch/arm64/boot/dts/qcom/*.dtbo
+
+cd $(pwd)/${OUT_DIR}/arch/arm64/boot/
+
+curl --upload-file dtbo.img https://transfer.sh/dtbo.img
+
+else
+exit 1;
