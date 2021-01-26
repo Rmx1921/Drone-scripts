6ef790d0f0da12b4d0f07b0b9677f12a09ba9192
viciouspup <yadhuuk@gmail.com>
2021-01-26 22:14:12 +0530

    Update setup-drone.sh

--- setup-drone.sh
+++ setup-drone.sh
@@ -18,8 +18,8 @@
 
 cd /drone/src/
 
-# dtbo files
-git clone https://github.com/utsavbalar1231/anykernel3 --depth=1 -b master anykernel
+# Anykernel
+git clone https://github.com/viciouspup/Anykernel.git --depth=1 -b master anykernel
 #git clone https://github.com/viciouspup/libufdt-master-utils.git libufdt-master-utils
 
 # clone benzoclang-12.0
