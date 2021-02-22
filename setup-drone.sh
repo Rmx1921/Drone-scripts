fceac46ea7cb91303ff0d72bf5decdd45cde4cb5
viciouspup <yadhuuk@gmail.com>
2021-02-22 20:44:32 +0000

    add libufdt-master-utils pull

--- setup-drone.sh
+++ setup-drone.sh
@@ -20,7 +20,7 @@ cd /drone/src/
 
 # Anykernel
 git clone https://github.com/viciouspup/Anykernel.git anykernel
-#git clone https://github.com/viciouspup/libufdt-master-utils.git libufdt-master-utils
+git clone https://github.com/viciouspup/libufdt-master-utils.git libufdt-master-utils
 
 # clone benzoclang-12.0
 if [[ "$@" =~ "benzoclang"* ]]; then
